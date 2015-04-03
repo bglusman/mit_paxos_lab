463ef8e8c138b24dab873f367c92e4ad9b32e657
Brian Glusman <brian@glusman.me>
2015-04-03 11:46:14 -0400

    spaces not tabs

--- master.go
+++ master.go
@@ -5,9 +5,9 @@ import "fmt"
 // import "net"
 
 type WorkerInfo struct {
-	address string
-	currentJobId int
-	// You can add definitions here.
+  address string
+  currentJobId int
+  // You can add definitions here.
 }
 
 func (w *WorkerInfo) SetAddress(address string) {
@@ -20,62 +20,62 @@ func (w *WorkerInfo) Address() string {
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
 func (mr *MapReduce) KillWorkers() *list.List {
-	l := list.New()
-	for _, w := range mr.Workers {
-		DPrintf("DoWork: shutdown %s\n", w.address)
-		args := &ShutdownArgs{}
-		var reply ShutdownReply
-		ok := call(w.address, "Worker.Shutdown", args, &reply)
-		if ok == false {
-			fmt.Printf("DoWork: RPC %s shutdown error\n", w.address)
-		} else {
-			l.PushBack(reply.Njobs)
-		}
-	}
-	return l
+  l := list.New()
+  for _, w := range mr.Workers {
+    DPrintf("DoWork: shutdown %s\n", w.address)
+    args := &ShutdownArgs{}
+    var reply ShutdownReply
+    ok := call(w.address, "Worker.Shutdown", args, &reply)
+    if ok == false {
+      fmt.Printf("DoWork: RPC %s shutdown error\n", w.address)
+    } else {
+      l.PushBack(reply.Njobs)
+    }
+  }
+  return l
 }
 
 func (mr *MapReduce) RunWorker(workerId string,
-															 workerChannel chan string,
-															 jobQueue chan DoJobArgs,
-															 completeJobs chan int) {
-	jobArgs := <- jobQueue
-	reply := DoJobReply{}
-	call(workerId, "Worker.DoJob",jobArgs, &reply)
-	if reply.OK {
-		workerChannel <- workerId
-		count := <- completeJobs
-		completeJobs <- count + 1
-	} else {
-		jobQueue <- jobArgs
-	}
+                               workerChannel chan string,
+                               jobQueue chan DoJobArgs,
+                               completeJobs chan int) {
+  jobArgs := <- jobQueue
+  reply := DoJobReply{}
+  call(workerId, "Worker.DoJob",jobArgs, &reply)
+  if reply.OK {
+    workerChannel <- workerId
+    count := <- completeJobs
+    completeJobs <- count + 1
+  } else {
+    jobQueue <- jobArgs
+  }
 
 }
 func (mr *MapReduce) RunMaster() *list.List {
 
-	jobQueue := make(chan DoJobArgs)
-	completeJobs := make(chan int)
-	go func(){
-		for workerId := range mr.registerChannel {
-			go mr.RunWorker(workerId, mr.registerChannel, jobQueue, completeJobs)
-		}
-	}()
+  jobQueue := make(chan DoJobArgs)
+  completeJobs := make(chan int)
+  go func(){
+    for workerId := range mr.registerChannel {
+      go mr.RunWorker(workerId, mr.registerChannel, jobQueue, completeJobs)
+    }
+  }()
 
-	for n:=0;n < mr.nMap; n++ {
-		jobQueue <- DoJobArgs{mr.file, Map, n, mr.nReduce}
-	}
-	completeJobs <- 0
-	fmt.Println("created map jobs:", mr.nMap)
-	for completeJobsCount := range completeJobs {
-		fmt.Println("completeJobs:", completeJobsCount)
-		if completeJobsCount < mr.nMap {
-				completeJobs <- completeJobsCount
-			 } else { break }
+  for n:=0;n < mr.nMap; n++ {
+    jobQueue <- DoJobArgs{mr.file, Map, n, mr.nReduce}
+  }
+  completeJobs <- 0
+  fmt.Println("created map jobs:", mr.nMap)
+  for completeJobsCount := range completeJobs {
+    fmt.Println("completeJobs:", completeJobsCount)
+    if completeJobsCount < mr.nMap {
+        completeJobs <- completeJobsCount
+       } else { break }
 
-	}
-	for n:=0;n < mr.nReduce; n++ {
-		jobQueue <- DoJobArgs{mr.file, Reduce, n, mr.nMap}
-	}
-	fmt.Println("created reduce jobs:", mr.nReduce)
-	return mr.KillWorkers()
+  }
+  for n:=0;n < mr.nReduce; n++ {
+    jobQueue <- DoJobArgs{mr.file, Reduce, n, mr.nMap}
+  }
+  fmt.Println("created reduce jobs:", mr.nReduce)
+  return mr.KillWorkers()
 }
